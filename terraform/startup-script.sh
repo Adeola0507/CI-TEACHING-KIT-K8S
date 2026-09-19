2df6c5038c6efdddeffb19659e147be48610815f
Adeola0507 <adechulababe@gmail.com>
2026-09-19 19:15:22 +0100

    changed gke node machine type

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -17,7 +17,7 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 # Clone the repo and bring the stack up
 cd /opt 
 git clone ${repo_url} ci-teaching-kit-k8s
-cd ci-teaching-kit-k8s/CI-TEACHING-KIT-K8S
+cd ci-teaching-kit-k8s
 docker compose up -d --build
 
 # Note: this brings the stack up automatically on VM creation. Nexus and the
